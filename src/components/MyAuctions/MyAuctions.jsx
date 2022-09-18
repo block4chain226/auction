c0d656d0abbf9827c88d4459a9c03ffc8ee17ba2
Ivan <retouch226@gmail.com>
2022-09-18 18:13:56 +0300

    feat:add countdown timer, when the timer reaches the end, it shows withdraw button

--- src/components/MyAuctions/MyAuctions.jsx
+++ src/components/MyAuctions/MyAuctions.jsx
@@ -1,8 +1,28 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import MyButton from "../../Ui/MyButton/MyButton";
-import cl from ".//MyAuctions.module.css";
+import Countdown from "react-countdown";
+import cl from "./MyAuctions.module.css";
 
 const MyAuctions = ({ auction }) => {
+  const [time, setTime] = useState("");
+
+  function getTimeLeft() {
+    const test = 1663513695106;
+    const endtime = auction.totalTime;
+    const currentTime = new Date();
+    const leftTime = test - currentTime;
+    console.log(
+      "🚀 ~ file: MyAuctions.jsx ~ line 8 ~ timer ~ endtime",
+      new Date().getTime()
+    );
+    leftTime > 0 ? setTime(leftTime) : setTime(0);
+  }
+
+  useEffect(() => {
+    getTimeLeft();
+  }, [auction]);
   return (
     <>
       <div className={cl.col}>
@@ -13,9 +33,23 @@ const MyAuctions = ({ auction }) => {
           <div className={cl.info}>
             <div className={cl.title}>{auction.title}</div>
             <div className={cl.text}>{auction.text}</div>
-            <div className={cl.leftTime}></div>
+            <div className={cl.leftTime}>
+              {time > 0 ? (
+                <Countdown
+                  onComplete={() => setTime(0)}
+                  date={Date.now() + time}
+                />
+              ) : (
+                <MyButton>Withdraw</MyButton>
+              )}
+            </div>
             <div className={cl.timePassed}></div>
-            <div className={cl.bidder}></div>
+            <div className={cl.bidder}>
+              <label for="bidder">
+                bidder
+                <input name="bidder" />
+              </label>
+            </div>
             <div className={cl.stop}>
               <MyButton>Stop Auction</MyButton>
             </div>
